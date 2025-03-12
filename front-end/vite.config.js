07644e81eada4d94854a8bde178316ce21709063
goldentorcka <cumbemarlon68@@gmail.com>
2025-03-12 11:59:41 -0500

    otro commit

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'  // Importamos 'path' para poder resolver rutas absolutas
 
+// ------------------------
+import tailwindcss from "@tailwindcss/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   // Agregamos la sección 'resolve' para definir alias
   resolve: {
     alias: {
       // Se configura el alias '@' para apuntar a la carpeta 'src'
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), //en shadcn ui aparece doble comilla, aca la tengo com comilla simple
     },
   },
 })
