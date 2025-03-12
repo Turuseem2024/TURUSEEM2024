fa296aea5094961cb0343f77e29839db2f59d405
goldentorcka <cumbemarlon68@@gmail.com>
2025-03-12 07:20:54 -0500

    Se intenta implementar shadcn ui desde cero en la parte IU para agregar los componentes

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'  // Importamos 'path' para poder resolver rutas absolutas
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Agregamos la sección 'resolve' para definir alias
+  resolve: {
+    alias: {
+      // Se configura el alias '@' para apuntar a la carpeta 'src'
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
