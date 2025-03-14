7447f639a69a4b8ecda3596aa0d8515278fe9868
goldentorcka <cumbemarlon68@@gmail.com>
2025-03-14 09:14:25 -0500

    se corrige el error con la configuracion de TAILWINDCSS en el archivo vite.config.js

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'  // Importamos 'path' para poder resolver rutas absolutas
+import path from 'path'
 
-// ------------------------
-import tailwindcss from "@tailwindcss/vite"
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // Agregamos la sección 'resolve' para definir alias
+  plugins: [react()], // Elimina tailwindcss() de aquí
   resolve: {
     alias: {
-      // Se configura el alias '@' para apuntar a la carpeta 'src'
-      '@': path.resolve(__dirname, 'src'), //en shadcn ui aparece doble comilla, aca la tengo com comilla simple
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
